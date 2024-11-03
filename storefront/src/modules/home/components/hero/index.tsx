0d486fb6dbc63d5d59204b9e7d54b254cd02bf2a
oshuncruz <37771084+oshuncruz@users.noreply.github.com>
2024-11-02 21:59:02 -0500

    changes to ui 2

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -4,15 +4,16 @@ import { Button, Heading } from "@medusajs/ui";
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        
-        {/* Centered Image */}
-        <img 
-          src="https://clothesrewear.s3.us-east-2.amazonaws.com/rewear.png" 
-          alt="Rewear Logo"
-          className="w-64 h-auto object-contain" // Adjust the width as needed
-        />
-
+      <div
+        className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6"
+        style={{
+          backgroundImage: `url("https://clothesrewear.s3.us-east-2.amazonaws.com/DCS08525-Enhanced-NR-2.jpg")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        {/* Optionally add other content here */}
       </div>
     </div>
   );
